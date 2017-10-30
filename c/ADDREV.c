5829ebbdecd07759165d18c5deca7b058d025c64
RavenKing144 <skaltheravenking@gmail.com>
2017-10-30 22:51:05 +0530

    add ADDREV.c

--- c/ADDREV.c
+++ c/ADDREV.c
@@ -1,31 +1,29 @@
-#include <stdio.h>
-
+#include<stdio.h>
 long numrev(long int x)
 {
-		int rem, rev=0;
-		while(x > 0)
+		int rem,rev=0;
+		while(x>0)
 		{
-			rem = x % 10;
-			rev = rev * 10 + rem;
-			x /= 10;
+			rem=x%10;
+			rev=rev*10+rem;
+			x/=10;
 		}
 		return rev;
 }
-
 int main()
 {
 	int N;
-	long int a, b, i;
+	long int a,b,c,d,e;
 	
-	scanf("%d", &N);
-	for(i = 0; i < N; i++)
+	scanf("%d",&N);
+	for(int i=0;i<N;i++)
 	{
-		scanf("%ld %ld", &a, &b);
-		a = numrev(a);
-		b = numrev(b);
-		a += b;
-		printf("%ld\n", numrev(a));
+		scanf("%d%d",&a,&b);
+		c=numrev(a);
+		d=numrev(b);
+		e=c+d;
+		printf("%l\n",numrev(e));
 	}
-
 	return 0;
 }
+
